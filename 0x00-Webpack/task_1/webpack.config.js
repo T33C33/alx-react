e34e142ce5fa0b6c751e6f27996e49879ad52c07
T33C33 <teeceeiheukwumere@gmail.com>
2024-11-19 15:14:00 +0100

    task_1_new

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -8,11 +7,4 @@ module.exports = {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'My Webpack App',
-      template: './public/index.html', // Optional: Specify a custom template
-      filename: 'index.html',
-    }),
-  ],
 };
