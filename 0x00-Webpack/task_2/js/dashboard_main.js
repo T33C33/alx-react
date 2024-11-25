3028ec917a03f8e47606e288fb22c553bafb251c
T33C33 <teeceeiheukwumere@gmail.com>
2024-11-25 08:50:58 +0100

    fixed

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -6,19 +6,19 @@ $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
-$('<div>', {
-  id: 'logo',
-  css: {
-    width: '200px',
-    height: '200px',
-  },
-}).prependTo('body');
+// $('<div>', {
+//   id: 'logo',
+//   css: {
+//     width: '200px',
+//     height: '200px',
+//   },
+// }).prependTo('body');
 
-document.getElementById('logo').style.backgroundImage =
-  'url("task_2/assets/holberton-logo.jpg")';
+// document.getElementById('logo').style.backgroundImage =
+//   'url("task_2/assets/holberton-logo.jpg")';
 
-document.getElementById('logo').style.width = '200px';
-document.getElementById('logo').style.height = '200px';
+// document.getElementById('logo').style.width = '200px';
+// document.getElementById('logo').style.height = '200px';
 
 let count = 0;
 
